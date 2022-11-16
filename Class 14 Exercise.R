6d3195751366a5228e982138297295796ea80751
sanaa-rashid19 <sanaa-rashid@outlook.com>
2022-11-16 14:10:41 -0500

    Initial commit

--- Class 14 Exercise.R
+++ Class 14 Exercise.R
@@ -8,7 +8,11 @@ usethis::create_github_token()
 
 gh::gh_whoami()
 
-usethis::use_git() 
-usethis::git_sitrep() 
+usethis::use_git_config(user.name="sanaa-rashid19",
+user.email="sanaa-rashid@outlook.com")
 
-usethis::use_github()
+usethis::use_git() 
+
+usethis::use_readme_md()
+
+usethis::use_ccby_license()
