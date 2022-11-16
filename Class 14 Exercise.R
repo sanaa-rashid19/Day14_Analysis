5b424a99ef48d60ba9a24008131b56da95d20428
sanaa-rashid19 <sanaa-rashid@outlook.com>
2022-11-16 13:48:57 -0500

    Initial commit

---
+++ Class 14 Exercise.R
@@ -0,0 +1,14 @@
+install.packages("usethis")
+
+install.packages("devtools")
+
+library(usethis)
+
+usethis::create_github_token()
+
+gh::gh_whoami()
+
+usethis::use_git() 
+usethis::git_sitrep() 
+
+usethis::use_github()
